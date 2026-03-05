2a884cddcd3d9fe5f71cc6d17c0329fd59da82bf
Kanishk Pachory <98859888+Kanishkpachory@users.noreply.github.com>
2026-03-05 10:45:30 +0100

    Arthmatic operator practise

---
+++ pra.java
@@ -0,0 +1,9 @@
+public class pra {
+    public static void main(String arg[]){
+        int num1 = 7;
+        int num2 = 2;
+
+        double result = num1 / num2;
+        System.out.println(result);
+    }
+}
