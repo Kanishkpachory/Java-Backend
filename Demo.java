c1fdde270f6b59a65e2df03814c266de60e28d3e
Kanishk Pachory <98859888+Kanishkpachory@users.noreply.github.com>
2026-03-25 11:39:44 +0100

    Constructor

--- Demo.java
+++ Demo.java
@@ -1,42 +1,44 @@
-package Encapsulation;
+package Constructor;
 
 class Human{
-    // private int age = 11;
-    // private String name = " kanishk";
-
     private int age;
     private String name;
-    public int getAge() {
+
+    public Human(){                             //Default constructor ( no values passed in paramter)
+        System.out.println("in constructor");
+        age = 12;
+        name = "John";
+    }
+    public Human(int a , String n){             //paramterized constructor (we assign parameter to use in main class directly)
+        System.out.println("in constructor  - 2222");
+        this.age = a;
+        this.name = n;
+    }
+   
+    public int getAge(){
         return age;
     }
-    public String getName() {
+    public String getName(){
         return name;
     }
-    // public void setAge(int age , Human obj) {
-    //     Human obj2 = obj;
-    //     obj2.age = age;
-    // }
-    public void setAge(int age) {
-       this.age = age;
+    public void setAge(int age){
+        this.age = age;
     }
-    public void setName(String name) {
+    public void setName(String name){
         this.name = name;
     }
-
+    
 }
 
 public class Demo {
     public static void main(String arg[]){
         Human obj = new Human();
-        // human.ageobj
-        // human.name = "Navin";
+        Human obj2 = new Human(14 , "Kanishk");
+        // Human obj1 = new Human();
 
-        // obj.setAge(14 , obj);
-        obj.setAge(14 );
-        obj.setName("pachory");
-
+        // obj.setAge(14);
+        // obj.setName("kanishk");
         System.out.println(obj.getName() + " : " + obj.getAge());
-
-
+        System.out.println(obj2.getName() + " : " + obj2.getAge());
     }
 }
