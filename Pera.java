d2b20655a90eac313ae48017438cbd0784bffaeb
Kanishk Pachory <98859888+Kanishkpachory@users.noreply.github.com>
2026-04-06 19:50:52 +0200

    Packages

---
+++ Pera.java
@@ -0,0 +1,13 @@
+package PackagesDay7;
+
+public class Pera {
+    // public Pera(){
+    //     System.out.println("in class Practise");
+    // }
+    
+    static{
+        System.out.println("in static block of class Practise");
+    }
+
+    
+}
